3ecfd9138dee4c0300e46512839b99b6fc746a24
Hannah Greenwell <hannah.greenwell@gmail.com>
2022-01-01 19:21:43 +1100

    completed 06.js

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -2,33 +2,82 @@
 // http://localhost:3000/isolated/exercise/06.js
 
 import * as React from 'react'
-// 🐨 you'll want the following additional things from '../pokemon':
-// fetchPokemon: the function we call to get the pokemon info
-// PokemonInfoFallback: the thing we show while we're loading the pokemon info
-// PokemonDataView: the stuff we use to display the pokemon info
-import {PokemonForm} from '../pokemon'
+import {ErrorBoundary} from 'react-error-boundary'
+import {
+  PokemonForm,
+  PokemonInfoFallback,
+  PokemonDataView,
+  fetchPokemon,
+} from '../pokemon'
+
+const requestStatus = {
+  idle: 'IDLE',
+  pending: 'PENDING',
+  resolved: 'RESOLVED',
+  rejected: 'REJECTED',
+}
 
 function PokemonInfo({pokemonName}) {
-  // 🐨 Have state for the pokemon (null)
-  // 🐨 use React.useEffect where the callback should be called whenever the
-  // pokemon name changes.
-  // 💰 DON'T FORGET THE DEPENDENCIES ARRAY!
-  // 💰 if the pokemonName is falsy (an empty string) then don't bother making the request (exit early).
-  // 🐨 before calling `fetchPokemon`, clear the current pokemon state by setting it to null
-  // 💰 Use the `fetchPokemon` function to fetch a pokemon by its name:
-  //   fetchPokemon('Pikachu').then(
-  //     pokemonData => {/* update all the state here */},
-  //   )
-  // 🐨 return the following things based on the `pokemon` state and `pokemonName` prop:
-  //   1. no pokemonName: 'Submit a pokemon'
-  //   2. pokemonName but no pokemon: <PokemonInfoFallback name={pokemonName} />
-  //   3. pokemon: <PokemonDataView pokemon={pokemon} />
+  const [state, setState] = React.useState({
+    pokemon: null,
+    status: pokemonName ? requestStatus.pending : requestStatus.idle,
+    error: null,
+  })
+  const {pokemon, status, error} = state
 
-  // 💣 remove this
-  return 'TODO'
+  console.log('PokemonInfo render start')
+
+  React.useEffect(() => {
+    console.log('PokemonInfo useEffect')
+    if (!pokemonName) {
+      return
+    }
+    setState({status: requestStatus.pending})
+    fetchPokemon(pokemonName)
+      .then(pokemonData => {
+        setState({
+          pokemon: pokemonData,
+          status: requestStatus.resolved,
+        })
+      })
+      .catch(error => {
+        setState({
+          error: error,
+          status: requestStatus.rejected,
+        })
+      })
+  }, [pokemonName])
+
+  console.log('PokemonInfo render end')
+
+  if (status === requestStatus.idle) {
+    return 'Submit a pokemon'
+  }
+  if (status === requestStatus.pending) {
+    return <PokemonInfoFallback name={pokemonName} />
+  }
+  if (status === requestStatus.resolved) {
+    return <PokemonDataView pokemon={pokemon} />
+  }
+  if (status === requestStatus.rejected) {
+    throw error
+  }
+
+  throw new Error()
+}
+
+function FallbackComponent({error, resetErrorBoundary}) {
+  return (
+    <div role="alert">
+      There was an error:{' '}
+      <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
+      <button onClick={resetErrorBoundary}>Try again</button>
+    </div>
+  )
 }
 
 function App() {
+  console.log('App render start')
   const [pokemonName, setPokemonName] = React.useState('')
 
   function handleSubmit(newPokemonName) {
@@ -40,10 +89,37 @@ function App() {
       <PokemonForm pokemonName={pokemonName} onSubmit={handleSubmit} />
       <hr />
       <div className="pokemon-info">
-        <PokemonInfo pokemonName={pokemonName} />
+        <ErrorBoundary
+          FallbackComponent={FallbackComponent}
+          onReset={() => setPokemonName('')}
+          resetKeys={[pokemonName]}
+        >
+          <PokemonInfo pokemonName={pokemonName} />
+        </ErrorBoundary>
       </div>
     </div>
   )
 }
 
 export default App
+
+// class ErrorBoundary extends React.Component {
+//   state = {error: null}
+
+//   static getDerivedStateFromError(error) {
+//     return {error}
+//   }
+
+//   componentDidCatch(error, errorInfo) {
+//     // log error here
+//   }
+
+//   render() {
+//     const {error} = this.state
+//     if (error) {
+//       return <this.props.FallbackComponent error={error} />
+//     }
+
+//     return this.props.children
+//   }
+// }
